914a9c2255720a70a05a8426e874d793da9945ef
Dor256 <dorb@wix.com>
2026-01-06 13:00:37 +0200

    change colors

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -3,6 +3,13 @@ return {
     name = "onedark",
     priority = 1000, -- make sure it loads before other UI plugins
     config = function()
+        require("onedarkpro").setup({
+            highlights = {
+                Directory = { fg = "${purple}" },
+                NeoTreeDirectoryName = { fg = "${fg}" },
+                NeoTreeRootName = { fg = "${fg}" },
+            }
+        })
         vim.cmd.colorscheme("onedark")
     end,
 }
